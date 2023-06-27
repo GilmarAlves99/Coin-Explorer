6fc9066889d947c00ae99f0de1ee32d9a8f69bbb
Gilmar Alves <GilmarAlves914@gmail.com>
2023-06-27 01:05:28 -0300

    FEAT : Conteudo de apresentação

--- src/js/fetchApi.js
+++ src/js/fetchApi.js
@@ -1,32 +1,57 @@
-const all = 5;
+ const all = 5;
 
-const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${all}&page=1`;
-const divMoedas = document.getElementById("moedasAll");
+ const url = `https:api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${all}&page=1`;
+ const divMoedas = document.getElementById("moedasAll");
+ const listaApi = document.getElementById("lista")
 
-
-function buscaAPi() {
-    fetch(url).then(response => response.json())
-        .then(data => {
-            data.forEach(coin => {
-
-
-                const { name, image } = coin;
+ function buscaAPi() {
+     fetch(url).then(response => response.json())
+         .then(data => {
+             data.forEach(coin => {
 
 
-                divMoedas.innerHTML += `
-                <div class='moeda'>
+                 const { name, image } = coin;
+
+
+                 divMoedas.innerHTML += `
+                 <div class='moeda'>
+                     <div class='nome'>
+                         <span>${name}</span>
+                     </div>
+                     <div class='img'>   
+                         <img class="coin-image" src='${image}'>
+                     </div>
+                 </div>
+             `;
+
+             });
+         }).catch(error => {
+             console.log("deu erro" + error)
+         })
+ }
+
+
+
+ function listaApis() {
+     const urlAll = `https:api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1`;
+     fetch(urlAll)
+         .then(response => response.json())
+         .then(data => data.forEach(element => {
+             listaApi.innerHTML +=
+                 `   
+                 <div class='moedastotais'>
                     <div class='nome'>
-                        <span>${name}</span>
+                        <span>${element.name}</span>
                     </div>
-                    <div class='img'>   
-                        <img class="coin-image" src='${image}'>
+                    <div class='img'>
+                        <img class="coin-image" src='${element.image}'>
                     </div>
-                </div>
-            `;
+                </div>`
+         })).catch(error => {
+             console.log("deu erro" + error)
+         })
+ }
 
-            });
-        }).catch(error => {
-            console.log("deu erro" + error)
-        })
-}
-buscaAPi();
+
+ listaApis();
+ buscaAPi();
